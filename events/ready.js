ce66403cbfecf5aa4974309fb12d111d2d223c28
Ryan Shepard <ryan.shep21@gmail.com>
2024-06-20 06:53:23 +0000

    Bot update

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 const Viewed = require('../models/viewed');
-const { crystal_channel } = require('../config.json');
+const config = require('../config.json');
+const cron = require('cron');
 const { Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
@@ -10,20 +11,22 @@ module.exports = {
 			.setCustomId('crystal')
 			.setLabel('View Crystal')
 			.setStyle(ButtonStyle.Primary);
-		const crystalChannel = client.channels.cache.find(c => c.id == crystal_channel);
+		const crystalChannel = client.channels.cache.find(c => c.id == config.crystal_channel);
 
-		setInterval(async () => {
+		const crystal =  new cron.CronJob('30 15 * * *', async () => {
 			await Viewed.update({ value: 0 }, { where: { value: 1 } });
 			crystalChannel.messages.fetch({ limit: 1 }).then(messages => {
 				messages.forEach(message => {
 					message.delete();
 				});
 			});
-			client.channels.cache.find(c => c.id == crystal_channel).send({
+			crystalChannel.send({
 				content: 'Click to get the crystal of the day:',
 				components: [new ActionRowBuilder().addComponents(crystalButton)]
 			});
-		}, 1000 * 60 * 60 * 24);
+		});
+
+		crystal.start();
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
