a0cf581a22fa14a43e2da4592239c8b0a46a5f7f
Ryan Shepard <ryan.shep21@gmail.com>
2024-06-21 01:49:32 +0000

    User app tickle command fix

--- commands/user/tickel.js
+++ commands/user/tickel.js
@@ -6,9 +6,9 @@ module.exports = {
         "contexts": [1, 2]
     },
     async execute(interaction) {
-        await interaction.reply({content: 'https://media1.tenor.com/m/nl5AkvIm4GoAAAAC/tickle-feet.gif', ephemeral: true});
+        await interaction.reply({content: 'https://media1.tenor.com/m/nl5AkvIm4GoAAAAC/tickle-feet.gif'});
         for (let i = 1; i < 6; i++) {
-            await interaction.followUp({content: 'https://media1.tenor.com/m/nl5AkvIm4GoAAAAC/tickle-feet.gif', ephemeral: true});
+            await interaction.followUp({content: 'https://media1.tenor.com/m/nl5AkvIm4GoAAAAC/tickle-feet.gif'});
         }
     }
 }
