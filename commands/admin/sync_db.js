a9da06b7d222f08936a5df389740b823783f3514
Ryan Shepard <ryan.shep21@gmail.com>
2024-11-28 16:33:39 -0600

    Database sync moved from seperate file to command.

--- commands/admin/sync_db.js
+++ commands/admin/sync_db.js
@@ -1,35 +1,46 @@
+const fs = require('fs');
 const {guildId} = require('../../config.json');
 const config = require('../../config.json');
 const Viewed = require('../../models/viewed');
+const Crystals = require('../../models/crystals');
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { exec } = require('child_process');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('sync_db')
 		.setDescription('Adds new users to the database.'),
 	async execute(interaction) {
-		console.log(interaction.user);
 		if (interaction.member.roles.cache.has(config.adminRole)) {
 			// First we sync the db if nothing is populated.
-			const Viewed = require('../../models/viewed');
-			const Crystal = require('../../models/crystals');
+			fs.access('./database.sqlite', fs.constants.F_OK, async (err) => {
+				if(err) {
+					console.log("Creating sqlite file...");
+					
+					exec(`node syncDB.js`, (error) => {
+						if (error) {
+    						console.error(`exec error: ${error}`);
+    						return;
+  						}
+					});	
 
-			if (Viewed.findAndCountAll() == 0 && Crystal.findAndCountAll() == 0) {
-				Viewed.sync({force: true});
-				Crystal.sync({ force: true });
-			}
-
-			const guild = interaction.client.guilds.cache.get(guildId);
-        	const members = await guild.members.cache.map(async member => {
-				// First we delete all members for a refill.
-				await Viewed.truncate();
+        			await interaction.reply({content:'Database file created successfully, please run the command again.', ephemeral:true});
+				} else {
+					const guild = interaction.client.guilds.cache.get(guildId);
+        			await guild.members.cache.map(async member => {
+						// Then we delete all members for a refill.
+						await Viewed.truncate();
 			
-				const user = await Viewed.create({
-					name: member.user.username,
-					value: false
-				});
+						await Viewed.create({
+							name: member.user.username,
+							value: false
+						});
+					});
+
+
+        			await interaction.reply({content:'All users successfully added', ephemeral:true});
+				}
 			});
-        	await interaction.reply({content:'All users successfully added', ephemeral:true});
 		} else {
 			await interaction.reply({content:'You need to be an admin to use this.', ephemeral:true});
 		}
